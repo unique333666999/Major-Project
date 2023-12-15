9e5ce3168b926d6c3f8b94c33e7f81a57d533f2b
Git-Hub <github369963@gmail.com>
2023-12-15 18:41:43 +0530

    Add Project Files

---
+++ public/js/map.js
@@ -0,0 +1,16 @@
+// mapboxgl.accessToken = mapToken;
+// const map = new mapboxgl.Map({
+//   container: "map", // container ID
+//   style: "mapbox://styles/mapbox/streets-v12",
+//   center: listing.geometry.coordinates, // starting position [lng, lat]
+//   zoom: 9, // starting zoom
+// });
+
+// const marker = new mapboxgl.Marker({ color: "red" })
+//   .setLngLat(listing.geometry.coordinates) //Listing.geometry.coordinates
+//   .set.Popup(
+//     new mapboxgl.Popup({ offset: popupOffsets, className: "my-class" }).setHTML(
+//       `<h4>${listing.title}</h4><p>Exact Location will be provided after booking</p>`
+//     )
+//   )
+//   .addTo(map);
